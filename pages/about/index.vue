a40db7b9c9e86c0510c298f3716269f359ebd726
jattoabdul <jattoade@gmail.com>
2019-06-10 14:32:53 +0100

    update about text

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -8,13 +8,24 @@
       </div>
       <div class="about-main">
         <p>
-          I am a versatile fullstack software engineer and a tech evangelist. I’m constantly in pursuit of knowledge, not just to update myself but to improve the quality of the team I work with and better the lives of people around me.
+          I am a versatile software engineer and a tech evangelist. I hold a bachelor of technology's degree in Geology with about 4 years experience building incredible software products for globally distributed teams. designing and developing solutions to solve complex technical problems using programming languages/frameworks such as python/flask, ruby/ruby on rails, javascript/React.js and vue.js. I have also worked with databases such as postgreSQL, MySQL and mongoDB. I have built REST and graphQL endpoints alike.
         </p>
         <p>
-          I enjoy setting up opportunities for learning. Co-founded the Freecodecamp Ilorin Group, served as mentor in various djangogirls events, teencode and other meetups. I also write technical and non-technical articles alike.I am constantly in pursuit of knowledge and challenging opportunities, not just to positively impact my growth, but to improve the quality of the teams I work with and better the lives of people around me.
+          I am enthusiastic about developing practical business solutions using my skill sets. I am constantly in pursuit of knowledge, seeking a fast-paced, challenging opportunities and agile work environment, not just to push the boundaries of my knowledge and positively impact my growth but to improve the quality of the teams I work with and better the lives of people around me. #IKeepLearning
+        </p>
+        <p>
+          I enjoy setting up opportunities for learning. Co-founded the Freecodecamp Ilorin Group, served as mentor in various djangogirls events, teencode and other meetups. I also write technical and non-technical articles alike.
         </p>
         <p>
           When I am not writing code or participating in online coding / algorithm challenges/games, I like to engage in some sporting activities. I do a martial art-taekwondo for self defense and sometimes I can be found playing soccer or video games.
+        </p>
+        <p>
+          Programming Languages: Python, Ruby, Javascript <br>
+          Tech Stacks: Flask, Django, Ruby on Rails, Node/Express, React/Redux, Vuejs/Nuxt.js <br>
+          Databases: PostgreSQL, MySQL, MongoDB, Redis <br>
+          API Specs: REST, GraphQL <br>
+          Infrastructure: AWS, Heroku, Git, Cloudinary <br>
+          Continuous Integration: CircleCI, TravisCI, GitlabCI <br>
         </p>
       </div>
     </main>
