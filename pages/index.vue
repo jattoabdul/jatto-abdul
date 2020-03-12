c64f412cb9a91256553b87abf781d3e95ac10d9e
jattoabdul <jattoade@gmail.com>
2020-03-12 20:00:07 +0100

    update about me

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
           <br> AMINUJATTO
         </h1>
         <p class="about-headline">
-          I am a versatile fullstack software engineer and a tech evangelist. I hold a bachelor of technology’s degree in geology with about 4 years experience building incredible software products for globally distributed teams.
+          I am a versatile Senior Software Engineer and a Tech Evangelist. I hold a bachelor of technology's degree in Geology with 5+ years experience building incredible software products for globally distributed teams.
         </p>
 
         <nuxt-link to="/portfolio" class="btn view-portfolio">
